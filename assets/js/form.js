2b96e8936c323426d5a8e9394acefe4276934735
Marly Valcin <marlyvalcin@gmail.com>
2024-04-15 14:54:43 -0400

    added a toggle button to index.html and added styling to style.css and defined variables for the form in form.js

---
+++ assets/js/form.js
@@ -0,0 +1,13 @@
+const username = document.getElementById("username")
+const title = document.getElementById("title")
+const content = document.getElementById("content")
+const submit = document.getElementById("submit")
+
+
+
+submit.addEventListener("click", (event) => {
+    event.preventDefault;
+    
+})
+
+

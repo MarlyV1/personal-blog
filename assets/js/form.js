643415646571ed7a6512bbde15c725e56da804a3
Marly Valcin <marlyvalcin@gmail.com>
2024-04-19 08:14:14 -0400

    created an object in form.js for the user's form information

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,8 +1,24 @@
-const username = document.getElementById("username")
-const title = document.getElementById("title")
-const content = document.getElementById("content")
+let username = document.getElementById("username")
+let title = document.getElementById("title")
+let content = document.getElementById("content")
 const submit = document.getElementById("submit")
 const form = document.getElementById("form")
 
 
+// Object to store the user's information
+const formInfo = {
+    user: username.value,
+    userTitle: title.value,
+    userContent: content.value,
+}
 
+// window.localStorage.setItem("username", username.value);
+// window.localStorage.setItem("title", title.value);
+// window.localStorage.setItem("content", content.value);
+
+
+// function storedFormData() {
+//     localStorage.setItem("username", username.value);
+//     localStorage.setItem("title", title.value);
+//     localStorage.setItem("content", content.value);
+// }

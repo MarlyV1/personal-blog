51a38276fb6440c7e1f50e1530457311f051f010
Marly Valcin <marlyvalcin@gmail.com>
2024-04-19 13:33:23 -0400

    updated form info function in form.js and the display function in blog.js

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -3,16 +3,18 @@ const blogContent = document.querySelector(".content")
 const postedBy = document.querySelector(".posted-by")
 
 
-
-
 // Gets the user information from local storage
 function displayFormInfo(){
+
     const displayInfo = JSON.parse(localStorage.getItem("userInfo"))
+
     console.log(displayInfo)
 
-    blogTitle.textContent = displayInfo.userTitle;
-    blogContent.textContent = displayInfo.userContent
-    postedBy.textContent = displayInfo.user
+    // blogTitle.textContent = displayInfo.userTitle;
+    // blogContent.textContent = displayInfo.userContent
+    // postedBy.textContent = displayInfo.user
 }
+
+
 displayFormInfo();
 
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,22 +1,36 @@
 let username = document.getElementById("username")
 let title = document.getElementById("title")
 let content = document.getElementById("content")
-const submit = document.getElementById("submit")
 const form = document.getElementById("form")
-
 
 // Object to store the user's information
 
+// function formInfo(){
+//     let formInfo = {
+//     user: username.value,
+//     userTitle: title.value,
+//     userContent: content.value
+// };
+//    console.log(formInfo)
+
+//    localStorage.setItem("userInfo", JSON.stringify(formInfo));
+// }
+
+
 function formInfo(){
+
+    let formData = JSON.parse(localStorage.getItem("userInfo")) || [];
+
     let formInfo = {
     user: username.value,
     userTitle: title.value,
     userContent: content.value
 };
-   console.log(formInfo)
+   formData.push(formInfo)
 
-   localStorage.setItem("userInfo", JSON.stringify(formInfo));
+   localStorage.setItem("userInfo", JSON.stringify(formData));
 }
+
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();

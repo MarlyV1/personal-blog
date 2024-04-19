b7abb57f21c303204f8435a8dd359960541bb554
Marly Valcin <marlyvalcin@gmail.com>
2024-04-19 13:54:15 -0400

    updated the function in blog.js to display the array information using the innerHTML element and deleted the placeholder blog section in blog.html

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,18 +1,27 @@
 const blogTitle = document.querySelector(".blog-title-1")
 const blogContent = document.querySelector(".content")
 const postedBy = document.querySelector(".posted-by")
+const blogSection = document.querySelector("#blog-section")
 
 
-// Gets the user information from local storage
+// Gets the user information from local storage 
 function displayFormInfo(){
 
     const displayInfo = JSON.parse(localStorage.getItem("userInfo"))
 
     console.log(displayInfo)
 
-    // blogTitle.textContent = displayInfo.userTitle;
-    // blogContent.textContent = displayInfo.userContent
-    // postedBy.textContent = displayInfo.user
+    displayInfo.forEach(index => {
+
+        // Displays the informattion from each item in the array in local storage
+        blogSection.innerHTML += `
+        <section>
+        <h3 class="blog-title-1">${index.userTitle}</h3>
+        <p class="content">${index.userContent}</p>
+        <p class="posted-by">Posted By: ${index.user}</p>
+        </section>
+        `
+    });
 }
 
 
